d36d7cc114bf55935bb44e7682782c5ac75481d3
Jorge Trinidad <jorgemtrinidad@outlook.es>
2020-08-28 18:32:43 -0600

    Elaboración parcial de la vista del GRID

--- frontend/src/Component/GridCasos/GridCasos.js
+++ frontend/src/Component/GridCasos/GridCasos.js
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.css"
 
 const GridCasos = () => {
 
@@ -33,36 +35,59 @@ const GridCasos = () => {
     console.log(Data)
     return (
         <>
-        <section className="header-grid">
-            <div className="div-search">
-                <input placeholder="Buscar Caso"  className="grid__search" type='text' onChange={(e) => handleSearch(e)} />
-                <span className="icon-search"></span>
-            </div>
-            <Link className="grid__btn" to="/crearcasos">Crear Casos</Link>
+            <section className="header-grid">
+                <div className="container mt-5">
+                    <div className="col-md-12">
+                        <div className="col-md-6 form-group">
+                            <input type="text" className="form" placeholder="Buscar Caso"  onChange={(e) => handleSearch(e)} />
+                            <span className="icon-search"></span>
+                            <div style={{ float: 'right' }}>
+                         <Link style={{ color: '#778899' }} className="filtros__btn" to="CreateCasos/CreateCasos.js">Crear Casos</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
         </section>
-        <section className="filtros">
+            <section className="filtros">
+                <div className="container mt-5">
+                    <div className="col-md-12">
+                        <div className="col-md-6 ">
+                            <div className="col-md-12">
             <div>
-                <button onClick={()=> setFilter(2)} className="filtros__btn">Fecha</button>
-                <button onClick={()=> setFilter(4)} className="filtros__btn">Desistidos</button>
-                <button onClick={()=> setFilter(1)} className="filtros__btn">Edad</button>
+                <button type="button" className="btn btn-dark" style={{ background: '#FF00FF'}}  onClick={()=> setFilter(2)}>Fecha</button>
+                <button type="button" className="btn btn-dark" style={{ background: '#FF00FF' }} onClick={()=> setFilter(4)} >Desistidos</button>
+                <button type="button" className="btn btn-dark" style={{ background: '#FF00FF' }} onClick={()=> setFilter(1)} >Edad</button>
             </div>
             <div>
                 <div>
-                    <input onChange={(e)=>setRadio(e.target.value)} name="c"  htmlFor="check" type="radio" value="1"  />
-                    <label id="check">Ascendente </label>
+                     <input type="checkbox" onChange={(e)=>setRadio(e.target.value)} name="c"  htmlFor="check" value="1"  />
+                     <label id="check">Ascendente </label>
                 </div>
                 <div>
-                    <input onChange={(e)=>setRadio(e.target.value)} name="c" htmlFor="check1" type="radio" value="2" />
+                    <input type="checkbox" onChange={(e)=>setRadio(e.target.value)} name="c" htmlFor="check1"  value="2" />
                     <label id="check1">Descendente</label>
                 </div>
-            </div>
+                    </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>                    
+             
         </section>
-        <section>
+            <section>
+                <div className="container mt-5">
+                    <div className="col-md-12">
+                        <div className="col-md-6 ">
+                            
             {
                 Data.map((data, index) => (
-                <h1 key={index}>{data.caseNumber}</h1>
+                    <li  class="list-group-item" style={{ background: '#FF00FF'}} key={index}>{data.caseNumber}</li>
                 ))
-            }
+                    }
+                            </div>
+                        </div>
+                    </div>
+                
         </section>
         </>
     )
