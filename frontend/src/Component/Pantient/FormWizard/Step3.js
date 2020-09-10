e80d5bf66109940bf97d1a56252fdad6eadc8ac5
Bryan Herado Carias Cervantes <63031593+bhcc-developer@users.noreply.github.com>
2020-09-09 20:34:57 -0600

    Pacientes. implementando el wizard

---
+++ frontend/src/Component/Pantient/FormWizard/Step3.js
@@ -0,0 +1,138 @@
+import React, {useState, useEffect} from 'react'
+import CKEditor from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+const Step3 = ({handlerStesp3}) => {
+
+  const [uuidCity, setUuidCity] = useState('')
+  const [addressLine1, setAddressLine1] = useState('')
+  const [addressLine2, setAddressLine2] = useState('')
+  const [phoneNumber, setPhoneNumber] = useState('')
+  const [CkeditorComment, setCkeditorComment] = useState('')
+  const [Image, setImage] = useState('')
+
+  const aguardarStadoActual = () => {
+    const step3Data = {
+      uuidCity,
+      addressLine1,
+      addressLine2,
+      phoneNumber,
+      CkeditorComment
+    }
+
+    localStorage.setItem('step3', JSON.stringify(step3Data))
+  }
+
+  const obtenerEstadoActual = () => {
+    const {
+      uuidCity,
+      addressLine1,
+      addressLine2,
+      phoneNumber,
+      CkeditorComment,
+    } = JSON.parse(localStorage.getItem('step3'))
+
+    setUuidCity(uuidCity)
+    setAddressLine1(addressLine1)
+    setAddressLine2(addressLine2)
+    setPhoneNumber(phoneNumber)
+    setCkeditorComment(CkeditorComment)
+  }
+
+
+  useEffect(()=> {
+
+    const data = localStorage.getItem('step3')
+    if(data) {
+      if(data, uuidCity) {
+        aguardarStadoActual()
+      }else {
+        obtenerEstadoActual()
+      }
+    }else {
+      aguardarStadoActual()
+    }
+
+    if (Image || addressLine1) {
+      handlerStesp3({
+        uuidCity,
+        addressLine1,
+        addressLine2,
+        phoneNumber,
+        CkeditorComment,
+        Image
+      })
+    }
+  },[
+    uuidCity,
+    addressLine1,
+    addressLine2,
+    phoneNumber,
+    CkeditorComment,
+    Image
+  ])
+
+
+
+
+  return (
+    <div>
+      <label htmlFor="uuidCity">uuidCity</label>
+      <select 
+      name="uuidCity"
+      value={uuidCity}
+      onChange={(env) => setUuidCity(env.target.value)}
+      >
+        <option value="0">Seleccionar un valor</option>
+        <option value="001">Valor 1</option>
+      </select>
+
+      <label htmlFor="addressLine1">addressLine1</label>
+      <input 
+        id="addressLine1" 
+        type="text" 
+        name="addressLine1"
+        value={addressLine1}
+        onChange={(env)=> setAddressLine1(env.target.value)} 
+      />
+
+      <label htmlFor="addressLine2">addressLine2</label>
+      <input 
+        id="addressLine2" 
+        type="text" 
+        name="addressLine2" 
+        value={addressLine2}
+        onChange={(env) => setAddressLine2(env.target.value)}
+      />
+
+      <label htmlFor="phoneNumber">phoneNumber</label>
+      <input 
+        id="phoneNumber" 
+        type="text" 
+        name="phoneNumber" 
+        value={phoneNumber}
+        onChange={(env)=> setPhoneNumber(env.target.value)}
+      />
+
+      <CKEditor
+        editor={ClassicEditor}
+        data={CkeditorComment}
+        onInit={editor => {
+        }}
+        onChange={(event, editor) => {
+          const data = editor.getData();
+          setCkeditorComment(data)
+        }}
+      />
+
+      <label htmlFor="attachment">attachment</label>
+      <input 
+        id="attachment" 
+        type="file" 
+        onChange={(env) => setImage(env.target.value)}
+      />
+    </div>
+  )
+}
+
+export default Step3
--- frontend/src/Component/Pantient/Pantient.js
+++ frontend/src/Component/Pantient/Pantient.js
@@ -1,217 +1,78 @@
 import React, {useState, useEffect} from 'react'
-import CKEditor from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import './Styles.pantient.scss'
+import Navbar from '../Navbar/Navbar'
+import { Link } from 'react-router-dom'
 
-
-function Edad(FechaNacimiento) {
-
-    var fechaNace = new Date(FechaNacimiento);
-    var fechaActual = new Date()
-
-    var mes = fechaActual.getMonth();
-    var dia = fechaActual.getDate();
-    var año = fechaActual.getFullYear();
-
-    fechaActual.setDate(dia);
-    fechaActual.setMonth(mes);
-    fechaActual.setFullYear(año);
-
-    var edad = Math.floor(((fechaActual - fechaNace) / (1000 * 60 * 60 * 24) / 365));
-   
-    return edad;
-}
 
 const Pantient = () => {
 
-    const [CkeditorComment, setCkeditorComment] = useState('')
-    const [Image, setImage] = useState()
-    const [FechaNacimiento, setFechaNacimiento] = useState()
-    const [Mostrar, setMostar] = useState()
+	const [pantient, setPantient] = useState([])
 
-  
-    
-    const handleSubmit = (e) => {
-        e.preventDefault()
+	useEffect(()=> {
+		getPantient()
+	},[])
 
-        console.log( Edad(FechaNacimiento) )
+	const getPantient = async () => {
+		fetch('http://localhost:4000/persons/all')
+		.then(res => res.json())
+		.then( data => setPantient(data.data))
+		.catch( err => console.error(err))
+	}
 
-        let formData = new FormData()
+	return (
+		<>	
+			<Navbar />
+			<section className="ed-container mt-6">
+				<div className="ed-item ed-container">
+					<div className="ed-item s-50 flex flex-center">
+						<h1 className="pantient-title">Pacientes</h1>
+					</div>
+					<div className="ed-item s-50 flex flex-right flex-center">
+						<Link to="/createpantient" className="pantient-btncreate">Crear Paciente</Link>
+					</div>
+				</div>
+			</section>
+			
+			<section className="container">
+				<div className="item">
+					<select className=" search-order">
+						<option value="0" className="search-order__option">Order</option>
+						<option value="1" className="search-order__option">Ascendente</option>
+						<option value="2" className="search-order__option">Descendente</option>
+					</select>	
+					<input
+						className="search-pantient" 
+						type="search"
+						placeholder="Buscar Pacientes"
+					/>
+					<img className="icon-search" src="./search.svg" alt="search"/>
+				</div>
+			</section>
 
-        formData.append('firstName', e.target.firstName.value)
-        formData.append('secondName', e.target.secondName.value)
-        formData.append('lastName', e.target.lastName.value)
-        formData.append('secondLastName', e.target.secondLastName.value)
-        formData.append('marriedName', e.target.marriedName.value)
-        formData.append('bornDate', FechaNacimiento)
-        formData.append('mobilePhone', e.target.mobilePhone.value)
-        formData.append('email', e.target.email22.value)
-        formData.append('uuidReligion', e.target.uuidReligion.value)
-        
-        formData.append('firstNameFather', e.target.firstNameFather.value)
-        formData.append('secondNameFather', e.target.secondNameFather.value)
-        formData.append('lastNameFather', e.target.lastNameFather.value)
-        formData.append('secondLastNameFather', e.target.secondLastNameFather.value)
-        formData.append('firstNameMother', e.target.firstNameMother.value)
-        formData.append('secondNameMother', e.target.secondNameMother.value)
-        formData.append('lastNameMother', e.target.lastNameMother.value)
-        formData.append('secondLastNameMother', e.target.secondLastNameMother.value)
-        formData.append('firstNameExtra', e.target.firstNameExtra.value)
-        formData.append('secondNameExtra', e.target.secondNameExtra.value)
-        formData.append('lastNameExtra', e.target.lastNameExtra.value)
-        formData.append('secondLastNameExtra', e.target.secondLastNameExtra.value)
-        
-        formData.append('uuidCity', e.target.uuidCity.value)
-        formData.append('addressLine1', e.target.addressLine1.value)
-        formData.append('addressLine2', e.target.addressLine2.value)
-        formData.append('phoneNumber', e.target.phoneNumber.value)
-        
-        formData.append('comment', CkeditorComment)
-        formData.append('attachment', Image )
-
-        console.log(formData.getAll('bornDate'))
-
-        fetch('http://localhost:4000/person/create', {
-            method: 'POST',
-            // headers: {
-            //     "Content-Type": "multipart/form-data",
-            //     "Accept": "application/json",
-            //     "type": "formData"
-            // },
-            body: formData
-        })
-        .then(res => res.json())
-        .then(data => console.log(data))
-        .catch(err => console.log(err))
-
-    }
-
-    useEffect(()=> {
-        const edad = Edad(FechaNacimiento)
-
-        if(edad >=18 ) {
-            setMostar(false)
-        }else {
-            setMostar(true)
-        }
-
-    },[FechaNacimiento])
-
-    // useEffect(() => {
-    //     fetch('http://localhost:4000/religion/all')
-    // }, [input])
-
-    return (
-
-        <form onSubmit={(e) => handleSubmit(e)} >
-
-            <label htmlFor="firstName">firstName</label>
-            <input id="firstName" type="text" name="firstName"  />
-    
-            <label htmlFor="secondName">secondName</label>
-            <input id="secondName" type="text" name="secondName"  />
-    
-            <label htmlFor="lastName">lastName</label>
-            <input id="lastName" type="text" name="lastName"  />
-    
-            <label htmlFor="secondLastName">secondLastName</label>
-            <input id="secondLastName" type="text" name="secondLastName"  />
-    
-            <label htmlFor="marriedName">marriedName</label>
-            <input id="marriedName" type="text" name="marriedName"  />
-    
-            <label htmlFor="bornDate">bornDate</label>
-            <input id="bornDate" type="date" onChange={(e) => setFechaNacimiento(e.target.value) } />
-    
-            <label htmlFor="mobilePhone">mobilePhone</label>
-            <input id="mobilePhone" type="text" name="mobilePhone"  />
-    
-            <label htmlFor="email">email</label>
-            <input id="email" type="email" name="email22"  />
-
-            <label htmlFor="uuidReligion">uuidReligion</label>
-            <select  name="uuidReligion">
-                <option value="0">Seleccionar un valor</option>
-                <option value="001">Valor 1</option>
-            </select>
-            {
-                Mostrar 
-                ? <div>
-                <label htmlFor="firstNameFather">firstNameFather</label>
-                <input id="email" type="text" name="firstNameFather"  />
-
-                <label htmlFor="secondNameFather">secondNameFather</label>
-                <input id="email" type="text" name="secondNameFather"  />
-
-                <label htmlFor="lastNameFather">lastNameFather</label>
-                <input id="email" type="text" name="lastNameFather"  />
-
-                <label htmlFor="secondLastNameFather">secondLastNameFather</label>
-                <input id="email" type="text" name="secondLastNameFather"  />
-
-                <label htmlFor="firstNameMother">firstNameMother</label>
-                <input id="email" type="text" name="firstNameMother"  />
-
-                <label htmlFor="secondNameMother">secondNameMother</label>
-                <input id="email" type="text" name="secondNameMother"  />
-
-                <label htmlFor="lastNameMother">lastNameMother</label>
-                <input id="email" type="text" name="lastNameMother"  />
-
-                <label htmlFor="secondLastNameMother">secondLastNameMother</label>
-                <input id="email" type="text" name="secondLastNameMother"  />
-
-                <label htmlFor="firstNameExtra">firstNameExtra</label>
-                <input id="email" type="text" name="firstNameExtra"  />
-
-                <label htmlFor="secondNameExtra">secondNameExtra</label>
-                <input id="email" type="text" name="secondNameExtra"  />
-
-                <label htmlFor="lastNameExtra">lastNameExtra</label>
-                <input id="email" type="text" name="lastNameExtra"  />
-
-                <label htmlFor="secondLastNameExtra">secondLastNameExtra</label>
-                <input id="email" type="text" name="secondLastNameExtra"  />
-            </div>
-            : <h1></h1>
-
-            }
-           
-
-            <label htmlFor="uuidCity">uuidCity</label>
-            <select  name="uuidCity">
-                <option value="0">Seleccionar un valor</option>
-                <option value="001">Valor 1</option>
-            </select>
-
-            <label htmlFor="addressLine1">addressLine1</label>
-            <input id="email" type="text" name="addressLine1"  />
-
-            <label htmlFor="addressLine2">addressLine2</label>
-            <input id="email" type="text" name="addressLine2"  />
-
-            <label htmlFor="phoneNumber">phoneNumber</label>
-            <input id="email" type="text" name="phoneNumber"  />
-
-            <CKEditor
-                    editor={ ClassicEditor }
-                    data="<p>Hello from CKEditor 5!</p>"
-                    onInit={ editor => {
-                        // You can store the "editor" and use when it is needed.
-                        console.log( 'Editor is ready to use!', editor );
-                    } }
-                    onChange={ ( event, editor ) => {
-                        const data = editor.getData();
-
-                        setCkeditorComment(data)
-                    } }
-                />
-
-            <label htmlFor="attachment">attachment</label>
-            <input id="email" type="file" onChange={(e)=>setImage(e.target.files[0])} />
-
-            <button type="submit">Guardar Paciente</button>
-        </form>
-    )
+			<div className="ed-container">
+		    {
+					pantient.map(({id, mobilePhone, addressLine1, email, active}) => (
+						<div key={id} className="ed-item">
+							<article className="s-shadow-bottom">
+								<div className="ed-grid m-grid-5 s-gap-2 m-pxy-2 s-bg-white s-radius-tl s-radius-tr">
+									<div className="s-pxy-2 m-pxy-0 m-cols-3">
+										<h3>{id}</h3>
+										<p className="s-mb-0">{email}</p>
+										<p className="s-mb-0">{addressLine1}</p>
+										<p className="s-mb-0">{mobilePhone}</p>
+									</div>
+								</div>
+								<footer className="s-bg-grey s-cross-center s-pxy-2 s-radius-br s-radius-bl">
+									<p className="s-10" className="">{ active === 1 ? 'Activo' : 'Inactivo'}</p>
+									<Link to={'/updatepantient/'+id} className="button s-to-right">Editar</Link>
+								</footer>
+							</article>
+						</div>
+					))
+				}
+			</div>
+		</>
+	)
 }
 
 export default Pantient
