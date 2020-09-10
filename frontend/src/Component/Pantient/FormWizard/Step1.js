bd625001f1448e7ea38b334c3b14cd0e86452919
OscarSantiago45 <oscarsantiago45@hotmail.com>
2020-09-10 11:28:56 -0600

    Css_Crear_Paciente
    Realizé los estilos del wizar de crear paciente.

---
+++ frontend/src/Component/Pantient/FormWizard/Step1.js
@@ -0,0 +1,206 @@
+import React, {useState, useEffect} from 'react'
+import './Style.step.scss'
+
+const Step1 = ({handlerStesp1}) => {
+
+  const [firstName, setFirstName] = useState('')
+  const [secondName, setSecondName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [secondLastName, setSecondLastName] = useState('')
+  const [marriedName, setMarriedName] = useState('')
+  const [FechaNacimiento, setFechaNacimiento] = useState('')
+  const [mobilePhone, setMobilePhone] = useState('')
+  const [email, setEmail] = useState('')
+  const [uuidReligion, setUuidReligion] = useState('')
+
+
+
+  const aguardarStadoActual = () => {
+    const step1Data = {
+      firstName,
+      secondName, 
+      lastName,
+      secondLastName,
+      marriedName,
+      FechaNacimiento,
+      mobilePhone,
+      email,
+      uuidReligion
+    }
+
+    localStorage.setItem('step1', JSON.stringify(step1Data))
+  }
+
+  const obtenerEstadoActual = () => {
+    const {
+      firstName,
+      secondName, 
+      lastName,
+      secondLastName,
+      marriedName,
+      FechaNacimiento,
+      mobilePhone,
+      email,
+      uuidReligion
+    } = JSON.parse(localStorage.getItem('step1'))
+
+    setFirstName(firstName)
+    setSecondName(secondName)
+    setLastName(lastName)
+    setSecondLastName(secondLastName)
+    setMarriedName(marriedName)
+    setFechaNacimiento(FechaNacimiento)
+    setMobilePhone(mobilePhone)
+    setEmail(email)
+    setUuidReligion(uuidReligion)
+  }
+
+  useEffect(() => {
+    const data = localStorage.getItem('step1')
+    if(data) {
+      if(data, firstName) {
+        aguardarStadoActual()
+      }else {
+        obtenerEstadoActual()
+      }
+    }else {
+      aguardarStadoActual()
+    }
+    
+    if(firstName) {
+      handlerStesp1({
+        firstName,
+        secondName, 
+        lastName,
+        secondLastName,
+        marriedName,
+        FechaNacimiento,
+        mobilePhone,
+        email,
+        uuidReligion
+      })
+    }
+
+
+  }, [
+    firstName,
+    secondName, 
+    lastName,
+    secondLastName,
+    marriedName,
+    FechaNacimiento,
+    mobilePhone,
+    email,
+    uuidReligion,
+    handlerStesp1,
+    aguardarStadoActual
+  ])
+
+  return (
+    <div className ="ed-container">
+      <div className="ed-item ed-container">
+        <div className="ed-item s-50">
+          <label htmlFor="firstName">Primer Nombre</label>
+          <input placeholder="Primer Nombre"
+          id="firstName" 
+          type="text" 
+          name="firstName"  
+          onChange={(env)=> setFirstName(env.target.value)} 
+          value={firstName}
+          />
+        </div>
+  
+        <div className="ed-item s-50">
+        <label htmlFor="secondName">Segundo Nombre</label>
+        <input placeholder="Segundo Nombre"
+          id="secondName" 
+          type="text" 
+          name="secondName" 
+          onChange={(env)=> setSecondName(env.target.value)} 
+          value={secondName}
+        />
+        </div>
+      </div>
+        
+      <div className="ed-item ed-container">
+        <div className="ed-item s-50">
+          <label htmlFor="lastName">Primer Apellido</label>
+          <input placeholder="Primer Apellido"
+            id="lastName" 
+            type="text" 
+            name="lastName" 
+            onChange={(env)=> setLastName(env.target.value)} 
+            value={lastName}
+          />
+        </div>
+        <div className="ed-item s-50">
+          <label htmlFor="secondLastName">Segundo Apellido</label>
+          <input placeholder="Segundo Apellido"
+            id="secondLastName" 
+            type="text" name="secondLastName" 
+            onChange={(env)=> setSecondLastName(env.target.value)} 
+            value={secondLastName}
+          />
+        </div>
+      </div>
+
+      <div className="ed-item ed-container">
+        <div className="ed-item s-50">
+          <label htmlFor="marriedName">Apllido de Casada</label>
+          <input placeholder="Apellido de Casada"
+            id="marriedName" 
+            type="text" 
+            name="marriedName" 
+            onChange={(env)=> setMarriedName(env.target.value)} 
+            value={marriedName}
+          />
+        </div>
+        <div className="ed-item s-50">
+          <label htmlFor="bornDate">Fecha de Nacimiento</label>
+          <input 
+            id="bornDate" 
+            type="date" 
+            onChange={(env)=> setFechaNacimiento(env.target.value)}  
+            value={FechaNacimiento}
+          />
+        </div>
+      </div>
+
+      <div className="ed-item ed-container">
+        <div className="ed-item s-50">
+          <label htmlFor="mobilePhone">Número de Teléfono</label>
+          <input placeholder="Número de Teléfono"
+            id="mobilePhone" 
+            type="text" 
+            name="mobilePhone" 
+            onChange={(env) => setMobilePhone(env.target.value)} 
+            value={mobilePhone}
+          />
+        </div>
+        <div className="ed-item s-50">
+          <label htmlFor="email">Dirección de Email</label>
+          <input placeholder="Email"
+            id="email" 
+            type="email" 
+            name="email22" 
+            onChange={(env) => setEmail(env.target.value)} 
+            value={email}
+          />
+        </div>
+        <div className="ed-item s-50">
+          <label htmlFor="uuidReligion" >Religion </label>
+          <select class="select-css"
+            value={uuidReligion} 
+            name="uuidReligion" 
+            onChange={(env)=> setUuidReligion(env.target.value)}
+          >
+            <option value="0">Seleccionar un valor</option>
+            <option value="001">Valor 1</option>
+          </select>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Step1
---
+++ frontend/src/Component/Pantient/FormWizard/Step2.js
@@ -0,0 +1,260 @@
+import React, {useState, useEffect} from 'react'
+import './Style.step.scss'
+
+
+const Step2 = ({handlerStesp2}) => {
+
+  const [firstNameFather, setFirstNameFather] = useState('')
+  const [secondNameFather, setSecondNameFather] = useState('')
+  const [lastNameFather, setLastNameFather] = useState('')
+  const [secondLastNameFather, setSecondLastNameFather] = useState('')
+  const [firstNameMother, setFirstNameMother] = useState('')
+  const [secondNameMother, setSecondNameMother] = useState('')
+  const [lastNameMother, setLastNameMother] = useState('')
+  const [secondLastNameMother, setSecondLastNameMother] = useState('')
+  const [firstNameExtra, setFirstNameExtra] = useState('')
+  const [secondNameExtra, setSecondNameExtra] = useState('')
+  const [lastNameExtra, setLastNameExtra] = useState('')
+  const [secondLastNameExtra, setSecondLastNameExtra] = useState('')
+
+
+  const aguardarStadoActual = () => {
+    const step2Data = {
+      firstNameFather,
+      secondNameFather,
+      lastNameFather,
+      secondLastNameFather,
+      firstNameMother,
+      secondNameMother,
+      lastNameMother,
+      secondLastNameMother,
+      firstNameExtra,
+      secondNameExtra,
+      lastNameExtra,
+      secondLastNameExtra
+    }
+
+    localStorage.setItem('step2', JSON.stringify(step2Data))
+  }
+
+  const obtenerEstadoActual = () => {
+    const {
+      firstNameFather,
+      secondNameFather,
+      lastNameFather,
+      secondLastNameFather,
+      firstNameMother,
+      secondNameMother,
+      lastNameMother,
+      secondLastNameMother,
+      firstNameExtra,
+      secondNameExtra,
+      lastNameExtra,
+      secondLastNameExtra
+    } = JSON.parse(localStorage.getItem('step2'))
+
+    setFirstNameFather(firstNameFather)
+    setSecondNameFather(secondNameFather)
+    setLastNameFather(lastNameFather)
+    setSecondLastNameFather(secondLastNameFather)
+    setFirstNameMother(firstNameMother)
+    setSecondNameMother(secondNameMother)
+    setLastNameMother(lastNameMother)
+    setSecondLastNameMother(secondLastNameMother)
+    setFirstNameExtra(firstNameExtra)
+    setSecondNameExtra(secondNameExtra)
+    setLastNameExtra(lastNameExtra)
+    setSecondLastNameExtra(secondLastNameExtra)
+    
+  }
+
+
+
+  useEffect(() => {
+
+    const data = localStorage.getItem('step2')
+    if(data) {
+      if(data, firstNameFather) {
+        aguardarStadoActual()
+      }else {
+        obtenerEstadoActual()
+      }
+    }else {
+      aguardarStadoActual()
+    }
+
+    if(firstNameFather) {
+      handlerStesp2({
+        firstNameFather,
+        secondNameFather,
+        lastNameFather,
+        secondLastNameFather,
+        firstNameMother,
+        secondNameMother,
+        lastNameMother,
+        secondLastNameMother,
+        firstNameExtra,
+        secondNameExtra,
+        lastNameExtra,
+        secondLastNameExtra
+      })
+    }
+
+   
+  }, [
+    firstNameFather,
+    secondNameFather,
+    lastNameFather,
+    secondLastNameFather,
+    firstNameMother,
+    secondNameMother,
+    lastNameMother,
+    secondLastNameMother,
+    firstNameExtra,
+    secondNameExtra,
+    lastNameExtra,
+    secondLastNameExtra,
+    handlerStesp2
+  ])
+
+  return (
+    <div className="ed-container">
+      <div className="ed-item ed-container">
+        <div className="ed-item s-50">
+          <label htmlFor="firstNameFather">Primer Nombre del Padre</label>
+          <input placeholder="Primer Nombre del Padre"
+            id="firstNameFather" 
+            type="text"
+            onChange={(env) => setFirstNameFather(env.target.value)} 
+            name="firstNameFather"
+            value={firstNameFather}
+          />
+        </div>
+        <div className="ed-item s-50">
+          <label htmlFor="secondNameFather">Segundo Nombre del Padre</label>
+          <input placeholder="Segundo Nombre del Padre"
+            id="secondNameFather" 
+            type="text"
+            onChange={(env) => setSecondNameFather(env.target.value)} 
+            name="secondNameFather" 
+            value={secondNameFather}
+          />
+        </div>
+      </div>
+      <div className="ed-item ed-container">
+        <div className="ed-item s-50">
+          <label htmlFor="lastNameFather">Primer Apellido del Padre</label>
+          <input placeholder="Primer Apellido del Padre"
+            id="lastNameFather" 
+            type="text"
+            onChange={(env) => setLastNameFather(env.target.value)} 
+            name="lastNameFather" 
+            value={lastNameFather}
+          />
+        </div>
+        <div className="ed-item s-50">
+          <label htmlFor="secondLastNameFather">Segundo Apellido del Padre</label>
+          <input placeholder="Segundo Apellido del Padre"
+            id="secondLastNameFather" 
+            type="text"
+            onChange={(env) => setSecondLastNameFather(env.target.value)} 
+            name="secondLastNameFather"
+            value={secondLastNameFather} 
+          />
+        </div>
+      </div>
+
+      <div className="ed-item ed-container">
+        <div className="ed-item s-50">
+          <label htmlFor="firstNameMother">Primer Nombre de la Madre</label>
+          <input placeholder="Primer Nombre de la Madre"
+            id="firstNameMother" 
+            type="text"
+            onChange={(env) => setFirstNameMother(env.target.value)} 
+            name="firstNameMother" 
+            value={firstNameMother}
+          />
+        </div>
+        <div className="ed-item s-50">
+          <label htmlFor="secondNameMother">Segundo Nombre de la Madre </label>
+          <input placeholder="Segundo Nombre de la Madre"
+            id="secondNameMother" 
+            type="text"
+            onChange={(env) => setSecondNameMother(env.target.value)} 
+            name="secondNameMother" 
+            value={secondNameMother}
+          />
+        </div>
+      </div>
+      <div className="ed-item ed-container">
+        <div className="ed-item s-50">
+          <label htmlFor="lastNameMother">Primer Apellido de la Madre</label>
+          <input  placeholder="Primer Apellido de la Madre"
+            id="lastNameMother" 
+            type="text"
+            onChange={(env) => setLastNameMother(env.target.value)} 
+            name="lastNameMother" 
+            value={lastNameMother}
+          />
+        </div>
+        <div className="ed-item s-50">
+          <label htmlFor="secondLastNameMother">Segundo Apellido de la Madre</label>
+          <input placeholder="Segundo Apellido de la Madre"
+            id="secondLastNameMother" 
+            type="text"
+            onChange={(env) => setSecondLastNameMother(env.target.value)} 
+            name="secondLastNameMother" 
+            value={secondLastNameMother}
+          />
+        </div>
+      </div>
+      <div className="ed-item ed-container">
+        <div className="ed-item s-50">
+          <label htmlFor="firstNameExtra">Primer Nombre Extra</label>
+          <input placeholder="Primer Nombre Extra"
+            id="firstNameExtra" 
+            type="text"
+            onChange={(env) => setFirstNameExtra(env.target.value)} 
+            name="firstNameExtra" 
+            value={firstNameExtra}
+          />
+        </div>
+        <div className="ed-item s-50">
+          <label htmlFor="secondNameExtra">Segundo Nombre Extra</label>
+          <input placeholder="Segundo Nombre Extra"
+            id="secondNameExtra" 
+            type="text"
+            onChange={(env) => setSecondNameExtra(env.target.value)} 
+            name="secondNameExtra" 
+            value={secondNameExtra}
+          />
+        </div>
+      </div>
+
+      <div className="ed-item ed-container">
+        <div className="ed-item s-50">
+          <label htmlFor="lastNameExtra">Primer Apellido Extra</label>
+          <input placeholder="Primer Apellido Extra"
+            id="lastNameExtra" 
+            type="text"
+            onChange={(env) => setLastNameExtra(env.target.value)} 
+            name="lastNameExtra" 
+            value={lastNameExtra}
+          />
+        </div>
+        <div className="ed-item s-50">
+          <label htmlFor="secondLastNameExtra">Segundo Apellido Extra </label>
+          <input placeholder="Segundo Apellido Extra"
+            id="secondLastNameExtra" 
+            type="text"
+            onChange={(env) => setSecondLastNameExtra(env.target.value)} 
+            name="secondLastNameExtra" 
+            value={secondLastNameExtra}
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Step2
