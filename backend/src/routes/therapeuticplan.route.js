c9b42f6e646295683736e0f3e5307ec17ab8cdc1
Mynor2397 <marold97@outlook.com>
2020-09-14 17:55:00 -0600

    Documentación endopoints

--- backend/src/routes/therapeuticplan.route.js
+++ backend/src/routes/therapeuticplan.route.js
@@ -3,7 +3,7 @@ const handTherapeuticplan = require('../controllers/therapeuticplan_controller')
 const { uploadFieldsFilesS3, manyUploadFile, multipleUploadFile } = require('../middlewares/uploadfile')
 const { filesTherapeuticPlan } = require('../lib/utils/rextherapeuticplan.file')
 
-Router.post('/therapeuticplan/create/:uuid', uploadFieldsFilesS3.fields(filesTherapeuticPlan), manyUploadFile, handTherapeuticplan.create)
-Router.put('/therapeuticplan/update/:uuidCaseinitial/:uuid', uploadFieldsFilesS3.fields(filesTherapeuticPlan), multipleUploadFile, handTherapeuticplan.update)
-Router.get('/therapeuticplan/getmany', handTherapeuticplan.getManyTherapeutics)
+Router.post('/:uuid', uploadFieldsFilesS3.fields(filesTherapeuticPlan), manyUploadFile, handTherapeuticplan.create)
+Router.put('/:uuidCaseinitial/:uuid', uploadFieldsFilesS3.fields(filesTherapeuticPlan), multipleUploadFile, handTherapeuticplan.update)
+Router.get('/getmany', handTherapeuticplan.getManyTherapeutics)
 module.exports = Router;
