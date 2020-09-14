80d71d0a9eec7d4883718d551e0a8bf96e41ece6
Bryan Herado Carias Cervantes <63031593+bhcc-developer@users.noreply.github.com>
2020-09-14 16:40:25 -0600

    Comenzando Con el diagnostico

--- frontend/src/Component/Etapas/wizard/EtapaDiagnostico.js
+++ frontend/src/Component/Etapas/wizard/EtapaDiagnostico.js
@@ -1,14 +1,67 @@
 import React from 'react'
+import CKEditor from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 const EtapaDiagnostico = ({
   currentStep,
-  handleChange
+  handleChange,
+  uuidDSM5,
+  descriptionOfProblem,
+  descriptionOfProblemFile
 }) => {
   if (currentStep !== 2) {
     return null
   }
   return (
-      <h1>Etapa Diagnostico</h1>
+      <>
+      <div className="ed-item">
+      <label htmlFor="uuidReligion" >Religion </label>
+            <select className="select-css"
+              value={uuidDSM5 || 0}
+              name="uuidDSM5"
+              onChange={handleChange}
+            >
+              <option value="0">Seleccionar un valor</option>
+              {/* {
+                uuidDSM5.map(({ uuid, name }) => (
+                  <option key={uuid} value={uuid}>{name}</option>
+                ))
+              } */}
+            </select>
+      </div>
+      <div className="ed-item xl-50">
+				<CKEditor
+					editor={ClassicEditor}
+					data={descriptionOfProblem}
+					onInit={editor => {
+					}}
+					onBlur={(event, editor) => {
+						const data = {
+							value: editor.getData(),
+							is: true,
+							name: 'descriptionOfProblem'
+						}
+						handleChange(data)
+					}}
+				/>
+		
+				<div className="input-group mb-3">
+					<div className="custom-file">
+						<input type="file" className="custom-file-input" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01" 
+							name='premorbidPersonalityFile'
+							onChange={handleChange}
+							/>
+						<label className="custom-file-label" htmlFor="inputGroupFile01"> 
+							{
+								descriptionOfProblemFile 
+								? descriptionOfProblemFile.name || descriptionOfProblemFile
+								:'Selecciones archivo' 
+							}
+						</label>
+					</div>
+				</div>
+      </div>
+      </>
   )
 }
 
